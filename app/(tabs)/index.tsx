452ec33167daae58b2242e312469eaa1e8ba7a14
陶浩南 <taoaaron5@gmail.com>
2025-03-16 21:33:08 +1100

    background basic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,21 @@
 /*
  * @Date: 2025-03-16 15:59:56
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-16 17:06:59
+ * @LastEditTime: 2025-03-16 21:32:00
  * @FilePath: /The_Movie_App/app/(tabs)/index.tsx
  */
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
 
+// The Main Home Page
+import { images } from "@/constants/images";
+import { Link } from "expo-router";
+
+import { View, Text, Image, ScrollView } from "react-native";
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary font-bold">
-        Welcome To My Mobile Movie App!
-      </Text>
+    <View className="flex-1 bg-primary">
+      {/* 最低等级z */}
+      <Image source={images.bg} className="w-full absolute z-0" />
+      <ScrollView></ScrollView>
     </View>
   );
 }
