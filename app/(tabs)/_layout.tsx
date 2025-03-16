01ab5136f7d4ebce9f5f9e6da669e9e1deccd2f4
陶浩南 <taoaaron5@gmail.com>
2025-03-16 17:50:02 +1100

    the focus clicked

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,18 +5,25 @@ import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
 // reuse component
-const TabIcon = ({ focused, icon, title }) => {
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <>
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+          <Image source={icon} tintColor="#151312" className="size-5" />
+          <Text className="text-secondary text-base font-semibold ml-2">
+            {title}
+          </Text>
+        </ImageBackground>
+      </>
+    );
+  }
   return (
-    <>
-      <ImageBackground
-        source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
-        <Image source={icon} tintColor="#151312" className="size-5" />
-        <Text className="text-secondary text-base font-semibold ml-2">
-          {title}
-        </Text>
-      </ImageBackground>
-    </>
+    <View>
+      <Image source={icon} tintColor="#A8B85DB" />
+    </View>
   );
 };
 const _layout = () => {
