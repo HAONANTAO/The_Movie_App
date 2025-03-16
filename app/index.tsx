499ce60e51e9e50a260803d71aa2137595f03fef
陶浩南 <taoaaron5@gmail.com>
2025-03-16 16:01:43 +1100

    generally set up

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2025-03-16 15:59:56
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-03-16 16:01:28
+ * @FilePath: /The_Movie_App/app/index.tsx
+ */
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -7,9 +13,8 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      }}>
+      <Text>Welcome</Text>
     </View>
   );
 }
