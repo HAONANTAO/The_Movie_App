f43ccfb398efc5bd78e5040ea40ae635964183b5
陶浩南 <taoaaron5@gmail.com>
2025-03-16 16:24:41 +1100

    //personal customize colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 /*
  * @Date: 2025-03-16 15:59:56
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-16 16:15:34
+ * @LastEditTime: 2025-03-16 16:18:18
  * @FilePath: /The_Movie_App/app/_layout.tsx
  */
 import { Stack } from "expo-router";
-import "./global.css";
+import "./globals.css";
 export default function RootLayout() {
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,15 @@
 /*
  * @Date: 2025-03-16 15:59:56
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-16 16:01:28
+ * @LastEditTime: 2025-03-16 16:20:08
  * @FilePath: /The_Movie_App/app/index.tsx
  */
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Text>Welcome</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Welcome!</Text>
     </View>
   );
 }
