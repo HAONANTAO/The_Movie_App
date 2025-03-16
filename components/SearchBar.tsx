9d9cd6bb07a44f7c24ae2ff2d329edcdb6f311b4
陶浩南 <taoaaron5@gmail.com>
2025-03-16 21:49:28 +1100

    TextInput

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 import React from "react";
 import { icons } from "@/constants/icons";
 
 const SearchBar = () => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-      <Text className="text-white">SearchBar</Text>
       <Image
         source={icons.search}
         className="size-5"
         resizeMode="contain"
         tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={() => {}}
+        placeholder="Search..."
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
       />
     </View>
   );
