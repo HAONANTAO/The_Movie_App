4bc1647a5aa109374b8e89a76aede637a1dab2a1
陶浩南 <taoaaron5@gmail.com>
2025-03-16 21:24:51 +1100

    height fix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
-import { hidden } from "../../node_modules/nativewind/src/metro/picocolors";
+
 
 // reuse component
 const TabIcon = ({ focused, icon, title }: any) => {
@@ -12,7 +12,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
       <>
         <ImageBackground
           source={images.highlight}
-          className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
           <Image source={icon} tintColor="#151312" className="size-5" />
           <Text className="text-secondary text-base font-semibold ml-2">
             {title}
