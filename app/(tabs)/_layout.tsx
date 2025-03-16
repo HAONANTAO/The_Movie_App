48540c3e2285f1ae067f08b5f54993985a09446f
陶浩南 <taoaaron5@gmail.com>
2025-03-16 21:26:56 +1100

    color fix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,6 @@ import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
-
 // reuse component
 const TabIcon = ({ focused, icon, title }: any) => {
   if (focused) {
@@ -12,7 +11,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
       <>
         <ImageBackground
           source={images.highlight}
-          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden ">
           <Image source={icon} tintColor="#151312" className="size-5" />
           <Text className="text-secondary text-base font-semibold ml-2">
             {title}
@@ -23,7 +22,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
   }
   return (
     <View className="size-full justify-center items-center mt-4 rounded-full">
-      <Image source={icon} tintColor="#A8B85DB" className="size-5" />
+      <Image source={icon} tintColor="#FFFFFF" className="size-5" />
     </View>
   );
 };
