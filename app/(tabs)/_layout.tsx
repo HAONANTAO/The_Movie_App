bd62c04ac486fdcf9f9f5bf2bd489b9cac8b2ae3
陶浩南 <taoaaron5@gmail.com>
2025-03-16 17:34:05 +1100

    TabIcon extract

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
+const TabIcon = () => {
+  return( <>
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full min-w-[112px] min-h-[14px] mt-4 justify-center items-center rounded-full overflow-hidden">
+      <Image source={icons.home} tintColor="#151312" className="size-5" />
+      <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+    </ImageBackground>
+  </>)
+ 
+};
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <TabIcon />,
+        }}
       />
       <Tabs.Screen
         name="profile"
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+/*
+ * @Date: 2025-03-16 15:59:56
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-03-16 17:06:59
+ * @FilePath: /The_Movie_App/app/(tabs)/index.tsx
+ */
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-const index = () => {
+export default function Index() {
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">
+        Welcome To My Mobile Movie App!
+      </Text>
     </View>
   );
-};
-
-export default index;
-
-const styles = StyleSheet.create({});
+}
