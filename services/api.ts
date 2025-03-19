7d2ceffbd5575b25b77ad7fb6dffbe163559ae21
陶浩南 <taoaaron5@gmail.com>
2025-03-19 15:20:24 +1100

    fetchMovies api create

--- services/api.ts
+++ services/api.ts
@@ -1,21 +1,37 @@
 /*
  * @Date: 2025-03-19 14:53:42
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-19 14:53:56
+ * @LastEditTime: 2025-03-19 15:19:30
  * @FilePath: /The_Movie_App/services/api.ts
  */
-// const url =
-//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTRlMmEzYzhiZmZlNjA1YjEwZWEyZTkzNjBmMDA3YiIsIm5iZiI6MTc0MjM1NTQzMS4wOTQsInN1YiI6IjY3ZGEzYmU3NmI0OTAyNWViOGExZTZiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t_uCnt3dt2eiOwuZQrjyCNnkO_mISO9-sSqspAzbds4",
-//   },
-// };
 
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.error(err));
+export const TMDB_CONFIG = {
+  // BASIC prefix
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+// /discover/movie
+// movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+
+// 根据欢迎程度获取电影
+export const fetchMovies = async ({ query }: { query: string }) => {
+  // URL to fetch
+  const endpoint = "/discover/movie?sort_by=popularity.desc";
+  const response = await fetch(TMDB_CONFIG.BASE_URL + endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // 获取失败
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+  // 获取成功
+  const data = await response.json();
+  return data.results;
+};
