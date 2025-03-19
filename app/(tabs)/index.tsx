f3d08d75ada921f5396eb73bcc924022fab66620
陶浩南 <taoaaron5@gmail.com>
2025-03-19 16:41:48 +1100

    flat list take display on name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
 /*
  * @Date: 2025-03-16 15:59:56
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-19 16:27:36
+ * @LastEditTime: 2025-03-19 16:41:30
  * @FilePath: /The_Movie_App/app/(tabs)/index.tsx
  */
 
 // The Main Home Page
 import { images } from "@/constants/images";
 import { Link } from "expo-router";
-import { View, Text, Image, ScrollView, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
@@ -31,38 +38,46 @@ export default function Index() {
       <Image source={images.bg} className="w-full absolute z-0" />
 
       {/* scrollable content container */}
-      <ScrollView
+      {/* <ScrollView
         className="flex-1 px-5"
         // 表示隐藏垂直滚动条
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
-        {/* top icon */}
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {/* search bar */}
-        {/* movie loading */}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}> */}
+      {/* top icon */}
+      <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      {/* search bar */}
+      {/* movie loading */}
 
-        {/* 先检查是不是加载，然后有没有错误再去显示 */}
-        {moviesLoading ? (
-          // 加载指示器组件
-          <ActivityIndicator
-            size="large"
-            color="#0000ff"
-            className="mt-10 self-center"
+      {/* 先检查是不是加载，然后有没有错误再去显示 */}
+      {moviesLoading ? (
+        // 加载指示器组件
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+        />
+      ) : moviesError ? (
+        <Text>Error:{moviesError?.message}</Text>
+      ) : (
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for movies"
           />
-        ) : moviesError ? (
-          <Text>Error:{moviesError?.message}</Text>
-        ) : (
-          <View className="flex-1 mt-5">
-            <SearchBar
-              onPress={() => router.push("/search")}
-              placeholder="Search for movies"
-            />
-            <Text className="text-lg text-white font-bold mt-5 mb-3">
-              Latest Movies
-            </Text>
-          </View>
-        )}
-      </ScrollView>
+          <Text className="text-lg text-white font-bold mt-5 mb-3">
+            Latest Movies
+          </Text>
+          {/* 高性能列表组件，专门用于渲染长列表数据。
+            它的主要优点是只渲染当前可见区域的内容，
+            对于大量数据的展示非常高效。 */}
+          <FlatList
+            data={movies}
+            renderItem={({ item }) => (
+              <Text className="text-white text-sm">{item.title}</Text>
+            )}></FlatList>
+        </View>
+      )}
+      {/* </ScrollView> */}
     </View>
   );
 }
