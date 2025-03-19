7fc3abf337eda577737f915df6f9eab9adceb405
陶浩南 <taoaaron5@gmail.com>
2025-03-19 23:06:14 +1100

    prepare to build the save and profile page!!!

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,16 +1,20 @@
 /*
  * @Date: 2025-03-16 16:47:16
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-16 16:47:21
+ * @LastEditTime: 2025-03-19 23:05:50
  * @FilePath: /The_Movie_App/app/(tabs)/save.tsx
  */
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const save = () => {
   return (
-    <View>
-      <Text>save</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-10 " tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Save</Text>
+      </View>
     </View>
   );
 };
