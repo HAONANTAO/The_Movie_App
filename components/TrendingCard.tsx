3c7b05495f881fe21f53d0815477af582c5b5514
陶浩南 <taoaaron5@gmail.com>
2025-03-19 22:25:24 +1100

    start hiding status bar

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,12 +1,14 @@
 /*
  * @Date: 2025-03-19 22:08:46
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-03-19 22:16:47
+ * @LastEditTime: 2025-03-19 22:23:27
  * @FilePath: /The_Movie_App/components/TrendingCard.tsx
  */
 import { Link } from "expo-router";
 import React from "react";
-import { TouchableOpacity, Image, View } from "react-native";
+import { TouchableOpacity, Image, View, Text } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { images } from "@/constants/images";
 
 const TrendingCard = ({
   index,
@@ -21,8 +23,21 @@ const TrendingCard = ({
           resizeMode="cover"
         />
         <View className="absolute bottom-9 -left-3.5 px-2 ppy-1 rounded-full">
-          
+          {/* 排序 */}
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }>
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
         </View>
+        <Text className="font-bold text-sm mt-2 text-white" numberOfLines={2}>
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
