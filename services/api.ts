cf408dfa2a2885aaa48eb78946795be8fc281df7
陶浩南 <taoaaron5@gmail.com>
2025-03-19 14:55:53 +1100

    start to use the TMDB api

---
+++ services/api.ts
@@ -0,0 +1,21 @@
+/*
+ * @Date: 2025-03-19 14:53:42
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-03-19 14:53:56
+ * @FilePath: /The_Movie_App/services/api.ts
+ */
+// const url =
+//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTRlMmEzYzhiZmZlNjA1YjEwZWEyZTkzNjBmMDA3YiIsIm5iZiI6MTc0MjM1NTQzMS4wOTQsInN1YiI6IjY3ZGEzYmU3NmI0OTAyNWViOGExZTZiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t_uCnt3dt2eiOwuZQrjyCNnkO_mISO9-sSqspAzbds4",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
